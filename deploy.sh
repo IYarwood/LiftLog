bed1f174cbb46bed551d11f6ab57fe2499539027
IYarwood <ianzyarwood@gmail.com>
2026-06-28 17:33:41 -0400

    Updating directories

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ echo "→ Pulling latest changes..."
 git pull
 
 echo "→ Installing dependencies..."
-cd /root/work/LiftLog/backend && npm install --omit=dev
+cd /var/work/LiftLog/backend && npm install --omit=dev
 
 echo "→ Restarting app..."
 pm2 restart liftlog
